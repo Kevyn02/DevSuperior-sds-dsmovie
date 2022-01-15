fc1fda6a6fd49358e10f0493c47f70e763c65cb9
Kevyn02 <kaparecido483@gmail.com>
2022-01-15 19:28:10 -0300

    Props

--- frontend/src/pages/Listing/Listing.tsx
+++ frontend/src/pages/Listing/Listing.tsx
@@ -7,21 +7,29 @@ import { Pagination } from "components/Pagination";
 import { Movie, MoviePage } from "types/movie";
 
 export function Listing() {
-  const [pageNumber, setPageNumber] = useState(0);
-
-  api.get<MoviePage>("/movies?size=12&page=0").then((response) => {
-    console.log(response.data);
-    setPageNumber(response.data.totalPages);
-  });
+  const [data, setData] = useState<MoviePage | undefined>();
+  const [currentPage, setCurrentPage] = useState(0);
+  useEffect(() => {
+    api
+      .get<MoviePage>(`/movies?size=12&page=${currentPage}&sort=title`)
+      .then((response) => {
+        // console.log(response.data);
+        setData(response.data);
+      });
+  }, [data?.number]);
 
   return (
     <>
       <Pagination />
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3">
-            <MovieCard />
-          </div>
+          {data?.content.map((movie) => {
+            return (
+              <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3">
+                <MovieCard movie={movie} />
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
