e43512293a5f6fad950fa45a885dc4482b00b9c2
Kevyn02 <kaparecido483@gmail.com>
2022-01-15 20:13:24 -0300

    Show score

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -5,14 +5,31 @@ import { ReactComponent as StarEmpty } from "assets/img/starEmpty.svg";
 import { ReactComponent as StarHalf } from "assets/img/starHalf.svg";
 import { ReactComponent as StarFull } from "assets/img/starFull.svg";
 
-export function MovieStars() {
+interface Props {
+  score: number;
+}
+export function MovieStars({ score }: Props) {
+  const qtStarFull = Math.floor(score / 1);
+  const qtStarHall = score % 1 ? 1 : 0;
+  const qtStarEmpty = 5 - qtStarFull - qtStarHall;
+
   return (
     <div className="dsmovie-stars-container">
-      <StarFull />
-      <StarFull />
-      <StarFull />
-      <StarHalf />
-      <StarEmpty />
+      {Array(qtStarFull)
+        .fill(0)
+        .map(() => {
+          return <StarFull />;
+        })}
+      {Array(qtStarHall)
+        .fill(0)
+        .map(() => {
+          return <StarHalf />;
+        })}
+      {Array(qtStarEmpty)
+        .fill(0)
+        .map(() => {
+          return <StarEmpty />;
+        })}
     </div>
   );
 }
